9a573a6c38afe7306c063c486c0f3dbf8d963f64
João Barbosa <joaofernandomeireles@gmail.com>
2020-11-05 16:55:22 +0000

    Improved wall following

--- catkin_ws/src/srr_control/scripts/wall_follow.py
+++ catkin_ws/src/srr_control/scripts/wall_follow.py
@@ -4,44 +4,57 @@ import math
 import rospy
 
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
 class WallFollower:
-    _PROPOTIONAL_FACTOR = 0.075 # Propotional factor of the error between wall's and robot's angles.
+    _TARGET_DISTANCE = 0.2 # Desired distance between the wall and the robot
+
+    _PROPOTIONAL_FACTOR = 1.8 # Propotional factor of the error
 
     def __init__(self):
         self.rate = rospy.Rate(10)
 
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.update_scan)
-        self.odom_sub = rospy.Subscriber('odom', Odometry, self.update_twist)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
         self.scan = LaserScan(ranges=[math.inf])
-        self.twist = Twist()
 
         self.loop()
 
     def update_scan(self, data: LaserScan):
         self.scan = data
 
-    def update_twist(self, data: Odometry):
-        self.twist = data.twist.twist
-
     def loop(self):
         while not rospy.is_shutdown():
-            min_laser, min_angle = min((range, i) for (i, range) in enumerate(self.scan.ranges))
+            min_range, min_angle = min((range, i) for (i, range) in enumerate(self.scan.ranges))
+
+            angular_direction = -1 if min_angle > 180 else 1
+            right_angle = 270 if min_angle > 180 else 90
 
             new_twist = Twist()
             new_twist.linear.x = 0.4; new_twist.linear.y = 0.0; new_twist.linear.z = 0.0
 
-            if min_laser < 1:
-                delta_angle = min_angle - (270 if min_angle > 180 else 90)
-                rospy.loginfo(f'Lazor: {min_angle}')
+            if min_range < 1:
+                distance = self.scan.ranges[right_angle] - self._TARGET_DISTANCE
+                delta_angle = (min_angle - right_angle) * math.pi / 180
 
-                new_twist.angular.x = 0.0; new_twist.angular.y = 0.0; new_twist.angular.z = self._PROPOTIONAL_FACTOR * delta_angle 
+                # if min_angle > 90 and min_angle < 270:
+                    # x_mirror_range = self.scan.ranges[right_angle - delta_angle]
+
+                    # if x_mirror_range < min_range + 0.1:
+                        # Compensate rotation on inner curve
+                        # delta_angle *= 2
+
+                # error_value = delta_angle
+                # error_value = angular_direction * distance
+                error_value = delta_angle + angular_direction * distance
+
+                # error_value = max(-2, min(error_value, 2))
+
+                new_twist.angular.x = 0.0; new_twist.angular.y = 0.0; 
+                new_twist.angular.z = self._PROPOTIONAL_FACTOR * error_value
                 self.cmd_vel_pub.publish(new_twist)
-
+            
             self.rate.sleep()
 
 
