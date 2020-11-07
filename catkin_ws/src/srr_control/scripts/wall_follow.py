87c79e26620bb6ee8b1915c25a2285478bba4273
João Barbosa <joaofernandomeireles@gmail.com>
2020-11-07 17:54:43 +0000

    Add ability to do the sharp turns nicely

--- catkin_ws/src/srr_control/scripts/wall_follow.py
+++ catkin_ws/src/srr_control/scripts/wall_follow.py
@@ -9,8 +9,8 @@ from geometry_msgs.msg import Twist
 class WallFollower:
     _TARGET_DISTANCE = 0.2      # Desired distance between the wall and the robot
 
-    _PROPOTIONAL_FACTOR = 2  # Propotional factor of the error
-    _INTEGRAL_FACTOR = 0.1     # Integral factor of the error
+    _ANGLE_FACTOR = 2.5  # Propotional factor of the error of the angle between wall and robot
+    _DISTANCE_FACTOR = 8  # Propotional factor of the error of the distance between wall and robot
 
     def __init__(self):
         self.rate = rospy.Rate(10)
@@ -19,8 +19,6 @@ class WallFollower:
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
         self.scan = LaserScan(ranges=[math.inf])
-        self.error_integral = 0.0
-
         self.loop()
 
     def update_scan(self, data: LaserScan):
@@ -34,27 +32,18 @@ class WallFollower:
             right_angle = 270 if min_angle > 180 else 90
 
             new_twist = Twist()
-            new_twist.linear.x = 0.4; new_twist.linear.y = 0.0; new_twist.linear.z = 0.0
+            new_twist.linear.x = 0.2; new_twist.linear.y = 0.0; new_twist.linear.z = 0.0
 
             if min_range < 1:
-                # self.scan.ranges[right_angle]
-                distance = self.scan.ranges[right_angle] - self._TARGET_DISTANCE
-                delta_angle = (min_angle - right_angle) * math.pi / 180
+                distance = min_range - self._TARGET_DISTANCE
 
-                # if min_angle > 90 and min_angle < 270:
-                    # x_mirror_range = self.scan.ranges[right_angle - delta_angle]
-
-                    # if x_mirror_range < min_range + 0.1:
-                        # Compensate rotation on inner curve
-                        # delta_angle *= 2
-
-                # error_value = delta_angle
-                # error_value = angular_direction * distance
-                error_value = max(-3.5, min(delta_angle + angular_direction * distance, 3.5))
-                self.error_integral += max(-0.5, min(error_value * 0.1, 0.5))
+                if self.scan.ranges[0] < 1.75 * self._TARGET_DISTANCE:
+                    delta_angle = math.pi/2 if self.scan.ranges[90] > self.scan.ranges[270] else 3 * math.pi/2
+                else:
+                    delta_angle = (min_angle - right_angle) * math.pi / 180
 
                 new_twist.angular.x = 0.0; new_twist.angular.y = 0.0; 
-                new_twist.angular.z = self._PROPOTIONAL_FACTOR * error_value + self._INTEGRAL_FACTOR * self.error_integral
+                new_twist.angular.z = max(-3, min(self._ANGLE_FACTOR * delta_angle + self._DISTANCE_FACTOR * angular_direction * distance, 3))
                 self.cmd_vel_pub.publish(new_twist)
             
             self.rate.sleep()
@@ -62,7 +51,7 @@ class WallFollower:
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('srr_test')
+        rospy.init_node('srr_wall_follow')
         WallFollower()
         rospy.spin()
     except rospy.ROSInterruptException:
